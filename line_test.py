9160f5f7bc9ded570706eaf8a4b61b9f9cc5ee75
gpuenpipat <gpuenpipat@gmail.com>
2026-03-06 10:57:16 +0700

    line_test.py

--- line_test.py
+++ line_test.py
@@ -1,14 +1,14 @@
 import requests
 import re
+import os
 
 url = "https://xn--42cah7d0cxcvbbb9x.com/"
 
-headers = {
-    "User-Agent": "Mozilla/5.0"
-}
-
-res = requests.get(url, headers=headers)
-html = res.text
+try:
+    res = requests.get(url, headers={"User-Agent": "Mozilla/5.0"}, timeout=10)
+    html = res.text
+except:
+    html = ""
 
 buy_match = re.search(r'รับซื้อ\s*([\d,]+)', html)
 sell_match = re.search(r'ขายออก\s*([\d,]+)', html)
@@ -28,20 +28,16 @@ Source: ราคาทอง.com
 else:
     message = "Gold Price Update\n\nUnable to detect gold price today."
 
-LINE_TOKEN = "rY8xBYsQpEpLrXDViRB2eC6aYrsttGkPUSx0+1hk1xPW6YhjyOft/6U0mf6IGRzrt82kcVeSQZOOU4MgM5+2OAiobJjhoRUSiEjX1fgkI5cqDhpy1ed8NBrAPaCzbNqQ4cj+1dvkBApinXCZ0zwA8AdB04t89/1O/w1cDnyilFU="
+LINE_TOKEN = os.getenv("nVV9K+Xb5Myd6hjkVWkkA/JbiDR4V+LUjfXS8mJfhMIQElpvcP/BTgtutIeA3Z52t82kcVeSQZOOU4MgM5+2OAiobJjhoRUSiEjX1fgkI5dt5E5Vc/bCyj3H4QkmRfl468zLrr5nkXwN9DblPIpx3QdB04t89/1O/w1cDnyilFU=")
 
-headers = {
-    "Authorization": f"Bearer {LINE_TOKEN}"
-}
+try:
+    requests.post(
+        "https://notify-api.line.me/api/notify",
+        headers={"Authorization": f"Bearer {LINE_TOKEN}"},
+        data={"message": message},
+        timeout=10
+    )
+    print("Message sent")
 
-data = {
-    "message": message
-}
-
-requests.post(
-    "https://notify-api.line.me/api/notify",
-    headers=headers,
-    data=data
-)
-
-print("Message sent")
+except Exception as e:
+    print("LINE send failed:", e)
