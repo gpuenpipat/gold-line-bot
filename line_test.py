9b8627577c49cd69d634dfbb89b8ad09863e87bb
gpuenpipat <gpuenpipat@gmail.com>
2026-03-06 13:34:43 +0700

    line_test.py

--- line_test.py
+++ line_test.py
@@ -1,16 +1,8 @@
 import requests
-from bs4 import BeautifulSoup
 import os
 
-URL = "https://xn--42cah7d0cxcvbbb9x.com/"
-
-response = requests.get(URL)
-soup = BeautifulSoup(response.text, "html.parser")
-
-price = soup.get_text()[:200]
-
 LINE_TOKEN = os.environ["nVV9K+Xb5Myd6hjkVWkkA/JbiDR4V+LUjfXS8mJfhMIQElpvcP/BTgtutIeA3Z52t82kcVeSQZOOU4MgM5+2OAiobJjhoRUSiEjX1fgkI5dt5E5Vc/bCyj3H4QkmRfl468zLrr5nkXwN9DblPIpx3QdB04t89/1O/w1cDnyilFU="]
-USER_ID = os.environ["Ub27b8ca8be36588af796a26e0a2b4af1"]
+LINE_USER = os.environ["Ub27b8ca8be36588af796a26e0a2b4af1"]
 
 headers = {
     "Authorization": f"Bearer {LINE_TOKEN}",
@@ -18,11 +10,11 @@ headers = {
 }
 
 data = {
-    "to": USER_ID,
+    "to": LINE_USER,
     "messages": [
         {
             "type": "text",
-            "text": f"Gold Price Update\n\n{price}"
+            "text": "Test message from GitHub Actions"
         }
     ]
 }
